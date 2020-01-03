c03f1965c9d6ec8b4fa4cf9f91b62f3fbfe09f3e
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-01-03 18:15:03 -0500

    Updates

---
+++ precache-manifest.be0556f75a619032449ea8a38ed03cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cda4d2bc8a8df4c477a9f1a8b17700d",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "2e4ebf58c84ddf3fc4e1",
+    "url": "/monsters-inc/static/css/main.e16f2831.chunk.css"
+  },
+  {
+    "revision": "938c5479c37b398100dc",
+    "url": "/monsters-inc/static/js/2.cfda392e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-inc/static/js/2.cfda392e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e4ebf58c84ddf3fc4e1",
+    "url": "/monsters-inc/static/js/main.7802e483.chunk.js"
+  },
+  {
+    "revision": "e080ef053613486469d6",
+    "url": "/monsters-inc/static/js/runtime-main.9fb7622c.js"
+  }
+]);
